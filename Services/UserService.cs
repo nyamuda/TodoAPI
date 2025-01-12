73d9810f32b85a4ab234ca9f369ee6f120fb70c6
P.T Nyamuda <ptnrlab@gmail.com>
2025-01-12 03:45:46 +0200

    User registration added

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,4 +1,7 @@
 ﻿using TodoAPI.Data;
+using TodoAPI.Dtos;
+using TodoAPI.Models;
+using BCrypt.Net;
 namespace TodoAPI.Services
 {
     public class UserService
@@ -8,6 +11,40 @@ namespace TodoAPI.Services
         public UserService(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+
+        public async Task<(bool isSuccess, string message)> RegisterUser(UserRegisterDto userRegisterDto)
+        {
+           try
+            {
+                //check if user with the provided email already exists
+                bool userExists = _context.Users.Any(u => u.Email.Equals(userRegisterDto.Email));
+                if (userExists) {
+                    var message = "User with that email already exists.";
+                    return (false, message);
+                }
+
+                //hash the password
+                var hashedPassword= BCrypt.Net.BCrypt.HashPassword(userRegisterDto.Password);
+
+                var user = new User
+                {
+                    Name = userRegisterDto.Name,
+                    Email = userRegisterDto.Email,
+                    Password = hashedPassword
+                };
+
+                //add the user
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return (true, "User has been registered");
+            }
+            catch(Exception ex)
+            {
+                return (false, ex.Message);
+            }
         }
     }
 }
