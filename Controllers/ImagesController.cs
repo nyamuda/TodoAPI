76fb9f8c29b3fd8cb4fe6772fba17590f201d843
P.T Nyamuda <ptnrlab@gmail.com>
2025-02-15 12:03:00 +0200

    images controller added

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Storage;
+using Microsoft.AspNetCore.Mvc;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly FirebaseStorage _firebaseStorage;
+
+        public ImagesController(FirebaseStorage firebaseStorage, IConfiguration config)
+        {
+            var bucket = config.GetSection("Authentication:Firebase:Bucket").Value;
+            _firebaseStorage = new FirebaseStorage(bucket);
+        }
+        // GET: api/<ImagesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ImagesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ImagesController>
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile file, IConfiguration config)
+        {
+           
+
+        }
+
+        // PUT api/<ImagesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ImagesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
