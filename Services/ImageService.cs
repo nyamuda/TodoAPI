0d3a57b64a77c57962ae504c7a276a5c5a5383fc
P.T Nyamuda <ptnrlab@gmail.com>
2025-02-17 11:48:37 +0200

    Checking if image is valid using Six Labors added

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -1,6 +1,31 @@
-﻿namespace TodoAPI.Services
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace TodoAPI.Services
 {
     public class ImageService
     {
+        public ImageService() { }
+
+        //check whether an image is a real and valid image or not
+        public bool IsImageValid(IFormFile file)
+        {
+            try
+            {
+                using var stream = file.OpenReadStream();
+                //if the file is not an a valid image
+                //then the following line will throw an exception
+                //e.g InvalidImageContentException
+                using var image = Image.Load(stream);
+
+                // If no exception is thrown, it's a valid image
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
     }
 }
