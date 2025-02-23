5c51d33f31ab026ed261a8e7667fb647c3f18afb
P.T Nyamuda <ptnrlab@gmail.com>
2025-02-23 17:21:45 +0200

    image model added

---
+++ Models/Image.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoAPI.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; } = default!;
+
+        public string FileName { get; set; }=default!;
+
+        public string? Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
+
+    }
+}
