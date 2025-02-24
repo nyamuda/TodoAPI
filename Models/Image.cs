daea67239b660d5e78ebbc6760606a4234dd029d
P.T Nyamuda <ptnrlab@gmail.com>
2025-02-24 07:07:15 +0200

    Firebase filepath added to Image model

--- Models/Image.cs
+++ Models/Image.cs
@@ -3,9 +3,11 @@
     public class Image
     {
         public int Id { get; set; }
-
-        public string Url { get; set; } = default!;
-
+        // Public URL for viewing
+        public string Url { get; set; } = default!; 
+        //Firebase storage path 
+        //We can use it if we want to delete the image on Firebase
+        public string FilePath { get; set; } = default!;  
         public string FileName { get; set; }=default!;
 
         public string? Category { get; set; }
