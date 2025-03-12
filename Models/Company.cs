865bf1c0b28e5f308d430a8141980ad01e7a6f6b
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-12 11:10:55 +0200

    company model added

---
+++ Models/Company.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoAPI.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public string Address { get; set; } = default!;
+
+        public string Phone { get; set; } = default!;
+
+        public DateOnly YearFounded { get; set; } = default!;
+    }
+}
