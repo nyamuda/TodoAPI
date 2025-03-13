b2f2f195c1a5a6bde27c64ac62bae595dcfb0697
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-13 06:22:56 +0200

    add  new company service added

--- Services/CompanyService.cs
+++ Services/CompanyService.cs
@@ -1,4 +1,6 @@
-﻿using TodoAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Data;
+using TodoAPI.Dtos.Company;
 using TodoAPI.Models;
 
 namespace TodoAPI.Services
@@ -10,14 +12,59 @@ namespace TodoAPI.Services
 
         public CompanyService(ApplicationDbContext context)
         {
-            _context=context;
+            _context = context;
         }
 
         //Get company by ID
         public async Task<Company> GetCompanyByID(int id)
         {
+            Company? company = await _context.Companies.FirstOrDefaultAsync(x => x.Id.Equals(id));
 
+            if (company is null) throw new KeyNotFoundException($"Company with ID {id} does not exist.");
 
+            return company;
+        }
+        //Company name is unique
+        //Get company by name
+        public async Task<Company> GetCompanyByName(string name)
+        {
+            Company? company = await _context.Companies.FirstOrDefaultAsync(x => x.Name.Equals(name));
+            if (company is null)
+                throw new KeyNotFoundException($"Company with name {name} does not exist.");
+
+            return company;
+
+        }
+
+        //Get all companies
+        public async Task<List<Company>> GetCompanies()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+
+        //Add company
+        public async Task<Company> AddCompany(CompanyDto companyDto)
+        {
+            //company name is unique
+            //check if there isn't a company that already has the given name
+            var companyName = companyDto.Name.ToLower();
+            Company? companyExists = await _context.Companies.FirstOrDefaultAsync(x => x.Name.Equals(companyName));
+            if (companyExists is not null)
+                throw new InvalidOperationException($"Company with name {companyDto.Name} already exists.");
+
+            var company = new Company()
+            {
+                Name = companyName,
+                Address = companyDto.Address,
+                Phone = companyDto.Phone,
+                YearFounded = companyDto.YearFounded
+
+            };
+
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return company;
 
         }
     }
