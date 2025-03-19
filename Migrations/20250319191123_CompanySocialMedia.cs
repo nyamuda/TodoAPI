8ced34e04450eb321b72036c77ca2a32f289b065
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-19 21:42:29 +0200

    default company opening hours added

---
+++ Migrations/20250319191123_CompanySocialMedia.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanySocialMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FacebookUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstagramUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedInUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpeningHours",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacebookUrl",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "InstagramUrl",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedInUrl",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningHours",
+                table: "Companies");
+        }
+    }
+}
