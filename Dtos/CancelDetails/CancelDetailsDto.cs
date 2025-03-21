635133a92a5b46ae027e55dd475e363e792e6136
P.T Nyamuda <ptnrlab@gmail.com>
2025-03-21 15:43:02 +0200

    CancellingUserDTO  added

---
+++ Dtos/CancelDetails/CancelDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using TodoAPI.Models;
+
+namespace TodoAPI.Dtos.CancelDetails
+{
+    public class CancelDetailsDto
+    {
+        public string CancelReason { get; set; } = default!;
+
+        public DateTime CancelledAt { get; set; } = default!;
+
+        public User CancelledByUser { get; set; } = default!;
+
+        
+        
+    }
+}
---
+++ Dtos/CancelDetails/CancellingUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAPI.Dtos.CancelDetails
+{
+    public class CancellingUserDTO
+    {
+        public string Name { get; set; }    
+
+        public string Role { get; set; }
+    }
+}
